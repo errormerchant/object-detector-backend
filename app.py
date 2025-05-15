3a6432be3a4f780e9190bbd696d42560608a86a2
errormerchant <advait.segfault@gmail.com>
2025-05-15 12:36:17 -0500

    Attempting Railway

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from PIL import Image
 import io
+import os
 from detector import detect_objects
 
 app = Flask(__name__)
@@ -19,4 +20,5 @@ def predict():
     return jsonify({"predictions": results})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
